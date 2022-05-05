fc0faed2d18164a5f61e59cfcc5f60d6d912eb50
ziyovuddin <ziyovuddinabdullayev487@gmail.com>
2022-05-05 12:37:30 +0500

    OCEAN

---
+++ js/script.js
@@ -0,0 +1,74 @@
+ 
+//    Introduction
+
+
+const btn=document.querySelector('.btn'),
+close=document.querySelector('.close'),
+box=document.querySelector('.box');
+
+btn.addEventListener('click',function(){
+    box.style.display='block';
+})
+close.addEventListener('click',function(){
+    box.style.display='none';
+})
+
+
+
+
+//Galleriy
+
+
+const btn1=document.querySelector('.btn1'),
+close1=document.querySelector('.close1'),
+box1=document.querySelector('.box1');
+
+btn1.addEventListener('click',function(){
+    box1.style.display='block';
+})
+close1.addEventListener('click',function(){
+    box1.style.display='none';
+})
+
+
+
+
+//  Test
+const btn2=document.querySelector('.btn2'),
+close2=document.querySelector('.close2'),
+box2=document.querySelector('.box2');
+
+btn2.addEventListener('click',function(){
+    box2.style.display='block';
+})
+close2.addEventListener('click',function(){
+    box2.style.display='none';
+})
+
+//About
+
+const btn3=document.querySelector('.btn3'),
+close3=document.querySelector('.close3'),
+box3=document.querySelector('.box3');
+
+btn3.addEventListener('click',function(){
+    box3.style.display='block';
+})
+close3.addEventListener('click',function(){
+    box3.style.display='none';
+})
+
+
+// ---------Container-----------
+
+const btn4=document.querySelector('.btn4'),
+close4=document.querySelector('.close4'),
+box4=document.querySelector('.box4');
+
+btn4.addEventListener('click',function(){
+    box4.style.display='block';
+})
+close4.addEventListener('click',function(){
+    box4.style.display='none';
+})
+
